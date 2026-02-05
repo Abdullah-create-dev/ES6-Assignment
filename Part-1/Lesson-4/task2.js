9e24de270016a9473895cba73b6186a0a650148e
abdullah <a21744693@gmail.com>
2026-02-06 02:07:38 +0500

    first commit

---
+++ Part-1/Lesson-4/task2.js
@@ -0,0 +1,13 @@
+let arr = ["toyota", "suzuki", "honda", "BMW", "audi"]
+
+for (let i = 0; i<arr.length; i++ ){
+    console.log(arr[i])
+}
+
+for(let i in arr){
+    console.log(arr[i])
+}
+
+for(let i of arr){
+    console.log(i)
+}
